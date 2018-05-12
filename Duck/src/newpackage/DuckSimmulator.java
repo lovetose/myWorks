81094604a0c01c9609e6776c5dbb324ae5746908
lovetose <tingtongkub5@hotmail.com>
2018-05-12 16:02:27 +0700

    Duck according to slide

---
+++ Duck/src/newpackage/DuckSimmulator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Student Lab
+ */
+public class DuckSimmulator {
+
+    public static void main(String args[]) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
---
+++ Duck/src/newpackage/FlyWithWings.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Student Lab
+ */
+public class FlyWithWings implements FlyBehavior{
+
+    public void fly() {
+        System.out.println("I'm flying!");
+    }
+}
